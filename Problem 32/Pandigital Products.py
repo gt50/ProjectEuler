f20f73881530e90c3b1df7caa0c73b04c5ab591e
gt50_ <gt50@hotmail.com>
2015-03-03 15:27:00 -0800

    problem 32 complete

--- Problem 32/Pandigital Products.py
+++ Problem 32/Pandigital Products.py
@@ -1,21 +1,30 @@
 __author__ = 'Shawn'
-
+import math
 
 def ispandigital(number):
-    #return sorted(map(int, str(number))) == list(set(map(int, str(number))))
-    return sorted(number) == list(set(number))
+    return sorted(map(int, str(number))) == range(1,len(str(number))+1) #list(set(map(int, str(number))))
 
-def getproducts(max):
-    for i in range(max):
-        for j in range(max):
-            if ispandigital(map(int, str(i)) + map(int, str(j)) + map(int, str(i*j))): print i, j, i*j
+def getproducts(maxdigits):
+    max_operand = (maxdigits -1)/2
+    pandigital_products = []
+    for i in range(1,int(math.pow(10,max_operand))):
+        for j in range(1,int(math.pow(10,max_operand))):
+            product_length = len(str(i)+str(j)+str(i*j))
+            if product_length > maxdigits:
+                break
+            if product_length == maxdigits:
+                if ispandigital(str(i) + str(j) + str(i*j)):
+                    pandigital_products.append(i*j)
+    print pandigital_products
+    print list(set(pandigital_products))
+    print sum(list(set(pandigital_products)))
 
 def test():
     assert ispandigital(15234) == True
-    assert ispandigital(7254) == True
+    assert ispandigital(391867254) == True
     assert ispandigital(1111) == False
     print "Tests Passed!"
 
 
 test()
-getproducts(10000)
+getproducts(9)
